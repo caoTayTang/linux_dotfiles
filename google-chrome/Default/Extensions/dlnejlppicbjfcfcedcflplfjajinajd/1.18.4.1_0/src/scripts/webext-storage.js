2a0f281b06843822143abc9d248f1ae56616de22
Kaios <ledai3062005@gmail.com>
2024-01-02 10:39:41 +0700

    removing google chrome and spotify

--- google-chrome/Default/Extensions/dlnejlppicbjfcfcedcflplfjajinajd/1.18.4.1_0/src/scripts/webext-storage.js
+++
@@ -1,4 +0,0 @@
-var startupStorage = {}
-
-chrome.storage.sync.get(null, (sync) => (startupStorage.sync = sync))
-chrome.storage.local.get(null, (local) => (startupStorage.local = local))
--- google-chrome/Default/Extensions/pnnfemgpilpdaojpnkjdgfgbnnjojfik/6.38_0/extension.js
+++
@@ -1,5 +0,0 @@
-window.extVersion = '6.38';
-
-window.__appUrl_gmail = 'https://assets.streak.com/clientjs-loaders/gmailApp.js';
-window.__appUrl_sheets = 'https://assets.streak.com/clientjs-loaders/sheetsApp.js';
-window.__appUrl_sheetsStore = 'https://assets.streak.com/clientjs-loaders/sheetsStoreApp.js';
