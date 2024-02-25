18779c46405d62d02b94ab2ef97c393addc00c6a
Kaios <ledai3062005@gmail.com>
2024-02-25 23:25:04 +0700

    up git

--- nvim/init.lua
+++ nvim/init.lua
@@ -129,11 +129,73 @@ require('lazy').setup({
 
   },
 
+  -- {
+  --   "romgrk/barbar.nvim",
+  --   config = function()
+  --     require 'barbar'.setup {}
+  --   end,
+  -- },
   {
-    "romgrk/barbar.nvim",
+    'b0o/incline.nvim',
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "SmiteshP/nvim-navic"
+    },
+    opts = {},
     config = function()
-      require 'barbar'.setup {}
+      require 'incline'.setup({
+        require('incline').setup({
+          render = function(props)
+            local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+            local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
+            local modified = vim.bo[props.buf].modified and 'bold,italic' or 'bold'
+
+            local function get_git_diff()
+              local icons = { removed = "", changed = "", added = "" }
+              icons["changed"] = icons.modified
+              local signs = vim.b[props.buf].gitsigns_status_dict
+              local labels = {}
+              if signs == nil then return labels end
+              for name, icon in pairs(icons) do
+                if tonumber(signs[name]) and signs[name] > 0 then
+                  table.insert(labels, { icon .. signs[name] .. " ", group = "Diff" .. name })
+                end
+              end
+              if #labels > 0 then table.insert(labels, { "┊ " }) end
+              return labels
+            end
+            local function get_diagnostic_label()
+              local icons = { error = '', warn = '', info = '', hint = '', }
+              local label = {}
+
+              for severity, icon in pairs(icons) do
+                local n = #vim.diagnostic.get(
+                  props.buf,
+                  { severity = vim.diagnostic.severity[string.upper(severity)] }
+                )
+                if n > 0 then
+                  table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
+                end
+              end
+              if #label > 0 then table.insert(label, { "┊ " }) end
+              return label
+            end
+
+            local buffer = {
+              { get_diagnostic_label() },
+              { get_git_diff() },
+              { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
+              { filename .. " ", gui = modified },
+              { "┊  " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" },
+            }
+            return buffer
+          end,
+
+        })
+      })
     end,
+    -- Optional: Lazy load Incline
+    --event = 'VeryLazy',
   },
 
   -- {
@@ -399,6 +461,20 @@ require('lazy').setup({
     build = 'pnpm add -g live-server',
     cmd = { 'LiveServerStart', 'LiveServerStop' },
     config = true
+  },
+  {
+    "andrew-george/telescope-themes",
+    cmd = 'Telescope',
+    lazy = true,
+    dependencies = {
+      'andrew-george/telescope-themes',
+      -- your other dependencies
+    },
+    config = function()
+      -- load extension
+      local telescope = require('telescope')
+      telescope.load_extension('themes')
+    end
   },
 
   require 'kickstart.plugins.autoformat',
